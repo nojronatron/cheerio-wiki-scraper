ec033685a55d6ee2bac03b6d036e80fe83de3ca5
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-11-07 15:54:47 -0800

    minor edits to home page jade file

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('rendering index');
   res.render('index', { title: 'Express' });
 });
 
